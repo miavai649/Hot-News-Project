48f144af21028936e4dde3b85564b954c34da5c2
Mahamudul Haque Noor <mahamudulhaquenoor@gmail.com>
2022-09-04 17:07:46 +0600

    categories btn added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
-console.log('mahmudul haque noor')
+const loadCategories = async () =>{
+  const url = `https://openapi.programming-hero.com/api/news/categories`;
+  try{
+    const res = await fetch(url);
+    const data = await res.json();
+    displayCategories(data.data.news_category);
+  }
+  catch(error){
+    console.log(error);
+  }
+};
+
+const displayCategories = async (categories) =>{
+  categories.forEach((category) => {
+    const {category_id, category_name} = category;
+
+    const categoryContainer = document.getElementById('category-container')
+    const li = document.createElement('li');
+    li.classList.add('text-md', 'lg:text-xl', 'font-semibold');
+    li.innerHTML=`
+    <a onclick="loadCategories(${category_id})">${category_name}</a>
+    `;
+
+    categoryContainer.appendChild(li);
+  });
+};
+
+
+
+
+
